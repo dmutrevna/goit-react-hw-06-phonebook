fcb7a432bf2278d0211650b47230b288de5728bf
dmutrevna <101204568+dmutrevna@users.noreply.github.com>
2023-08-29 13:19:12 +0200

    add loading and error

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
@@ -11,9 +11,12 @@ import {
   TitleContacts,
   TitlePhone,
 } from './ContactForm/ContactForm.styled';
+import { selectError, selectLoading } from 'redux/store/selectors';
 
 export const App = () => {
   const dispatch = useDispatch();
+  const isLoading = useSelector(selectLoading);
+  const error = useSelector(selectError);
 
   useEffect(() => {
     dispatch(getContactsThunk());
@@ -25,6 +28,8 @@ export const App = () => {
       <ContactForm />
       <TitleContacts>Contacts</TitleContacts>
       <Filter />
+      {isLoading && <p>Loading contacts...</p>}
+      {error && <p>An error occurred while loading contacts</p>}
       <ContactList />
     </AppContainer>
   );
